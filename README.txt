95c965a54bf059e408fad55f19d40ad477863cb6
Chester Holtz <chesterholtz@gmail.com>
2016-01-22 05:49:19 -0500

    fixing lambda

--- README.txt
+++ README.txt
@@ -3,13 +3,14 @@ Email: choltz2@u.rochester.edu
 Course: CSC246
 Homework: Implement linear regression for the adult income dataset using Python.
 ************ Files *********
-{Describe all files submitted}
+README.txt This file
+regression.py My implementation of a linear regression based classifier.
 
 ************ Algorithm *****
 {Describe your algorithm, using simple math notations}
 
 ************ Instructions ***
-{How to run your scripts}
+python regression.py {your test file}
 
 ************ Results *******
 {Report the result on dev and test. You should make it clear what’s you parameter selection procedure on the dev set. Don’t report all the console output, do it neat and clean. If you are doing comparison with previous homework, report the previous result on dev and test}
@@ -18,4 +19,4 @@ Homework: Implement linear regression for the adult income dataset using Python.
 {Analyze the result you have, try to explain how your machine learning algorithm works and the comparison with previous algorithms if required}
 
 ************ References ************
-{Any external materials you used for the homework. Anyone you discussed with}
+texbook
--- regression.py
+++ regression.py
@@ -1,24 +1,6 @@
 import numpy as np
+import sys
 # +1 5:1 8:1 18:1 22:1 36:1 40:1 51:1 61:1 67:1 72:1 75:1 76:1 80:1 83:1
-
-log = np.log
-
-def parse_line(line):
-    return 0
-
-def preparse(data):
-    w = []
-    y = []
-    for i, vector in enumerate(data):
-        w.append([])
-        for j in range(123):
-            w[i].append(0) # initialize vector
-        vector = vector.split()
-        y.append(int(vector.pop(0)))
-        for feature in vector:
-            t = feature.split(':')
-            w[i][int(t[0])] = 1
-    print y
 
 def load_file(fname):
     data = []
@@ -26,9 +8,46 @@ def load_file(fname):
         data = f.readlines()
     return data
 
+def preparse(data):
+    X = []
+    y = []
+    for i, vector in enumerate(data):
+        X.append([])
+        for j in range(124):
+            X[i].append(0) # initialize data matrix to zeroes
+        vector = vector.split()
+        y.append(int(vector.pop(0))) # pop our target vector
+        for feature in vector:
+            t = feature.split(':')
+            X[i][int(t[0])] = 1
+
+    X_mat = np.matrix(X, dtype=int) # convert our data array to matrix
+    y_mat = np.array(y,dtype = int) # convert our target to vector
+    return X_mat, y_mat
+
+def compute_model(X, y, l = 1):
+    n_col = X.shape[1]
+    f = np.linalg.lstsq(X.T.dot(X) + l * np.identity(n_col), np.squeeze(np.asarray(X.T.dot(y)))) # Regularized linear regression w/ normal equation
+    return f
+
 def main():
-    f = load_file("a7a.dev")
-    preparse(f)
+    f1 = load_file("a7a.train")
+    X, y= preparse(f1)
+    W = compute_model(X,y)
+
+    f2 = load_file(sys.argv[1])
+    X2, y2 = preparse(f2)
+    result = np.squeeze(np.asarray(X2)).dot(W[0]) # compute our result
+
+    correct = 0.0
+    for i in result: # calculate accuracy
+        if i > 0 and y2[i] == 1:
+            correct = correct + 1.0
+        if i < 0 and y2[i] == -1:
+            correct = correct + 1.0
+
+    print(str(correct) + " correct predictions for " + str(y2.shape[0]) + " points")   
+    print("The accuracy is " + str(correct/y2.shape[0]))   
 
 if __name__ == "__main__":
     main()
